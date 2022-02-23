0d3e5903928443b3178c051f87642e8b550f04e3
Jake Sims <jake.jake.sims@gmail.com>
2022-02-23 09:14:44 -0600

    first commit of just-my-type, some functionality in place.

---
+++ app.js
@@ -0,0 +1,79 @@
+
+
+
+
+
+
+$(document).ready(function () {
+    $("#keyboard-upper-container").hide();
+    $('#sentence').text(currentSentence);
+
+    let sentences = ['ten ate neite ate nee enet ite ate inet ent eate',
+        'Too ato too nOt enot one totA not anot tOO aNot',
+        'oat itain oat tain nate eate tea anne inant nean',
+        'itant eate anot eat nato inate eat anot tain eat',
+        'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+
+    var sentencesIndex = 0;
+    var lettersIndex = 0;
+    var currentSentence = sentences[sentencesIndex]
+    var letters = currentSentence.split('', currentSentence.length)
+    var currentLetter = letters[lettersIndex];
+    $('#target-letter').text(currentLetter);
+
+    $(document).keydown(function (e) {
+        if (e.which == 16) {
+            $("#keyboard-upper-container").show();
+            $("#keyboard-lower-container").hide();
+
+        } else {
+            if (lettersIndex < currentSentence.length) {
+            lettersIndex++;
+            currentLetter = letters[lettersIndex]; 
+            $('#target-letter').text(currentLetter);
+            }
+            if (lettersIndex == currentSentence.length) {
+                $('#sentence').text(" "); 
+                sentencesIndex ++; 
+                currentSentence = sentences[sentencesIndex];
+                $('#sentence').text(currentSentence);
+                lettersIndex = 0; 
+                letters = currentSentence.split('', currentSentence.length)
+                currentLetter = letters[lettersIndex];
+                $('#target-letter').text(currentLetter);
+            }
+            
+        };
+    });
+
+    $(document).keyup(function (e) {
+        if (e.which == 16) {
+            $("#keyboard-upper-container").hide();
+            $("#keyboard-lower-container").show();
+
+        };
+    });
+
+    $(document).keypress(function (e) {
+        let targetNum = e.key.charCodeAt();
+        $('#' + targetNum).css('background-color', 'yellow');
+        $(document).keyup(function (e) {
+            let targetNum = e.key.charCodeAt();
+            $('#' + targetNum).css('background-color', '');
+        });
+
+    });
+
+
+    screenSentence();
+
+    function screenSentence() {
+        $('#sentence').append(currentSentence);
+    }
+
+
+
+
+
+
+}); 
