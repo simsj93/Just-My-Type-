5d9e64afb5c8636320550f3240b804420b5c22e5
Jake Sims <jake.jake.sims@gmail.com>
2022-02-23 16:48:03 -0600

    finished cowboy themed typing game with only minimal missing functionality.

--- app.js
+++ app.js
@@ -7,48 +7,84 @@
 $(document).ready(function () {
     $("#keyboard-upper-container").hide();
     $('#sentence').text(currentSentence);
-    
 
-    const sentences = ['ten ate neite ate nee enet ite ate inet ent eate',
-        'Too ato too nOt enot one totA not anot tOO aNot',
-        'oat itain oat tain nate eate tea anne inant nean',
-        'itant eate anot eat nato inate eat anot tain eat',
-        'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+
+    const sentences = ["McCoy is in trouble down at the saloon again.",
+        "That McCoy just won't settle up his card debts!",
+        "Now one of those lowdown outlaws put a snake in his boots.",
+        "McCoy's angrier than a temperance nag in a whisky barrel.",
+        "He's calling for a showdown at high noon!"];
 
     var sentencesIndex = 0;
     var lettersIndex = 0;
     var currentSentence = sentences[sentencesIndex]
     var letters = currentSentence.split('', currentSentence.length)
     var currentLetter = letters[lettersIndex];
-    var blockPosition = 0; // number of pixels from the left for yellow block
-    $('#target-letter').text(currentLetter);
+    var blockPosition = 0; // number of pixels from the left for yellow block  
+    var correct = 0;
+    var mistake = 0;
+    
 
+    $('#target-letter').text(currentLetter);
+    $(document).keyup(function (e) {
+        if (sentencesIndex == 5) {
+          if  (confirm("Mighty fine keyslinging there, Partner. The number of misfires you made was only" + " " + mistake + "! Reckon you did it in no time flat too, but us cowpokes don't go in for fancy computer time-keeping. Press OK (Corral) to try again.") == true) {
+                window.location.reload(); 
+          } else {
+
+          }
+
+        }
+    })
+    $(document).keyup(function (e) {
+        if (blockPosition == 0) {
+            $('#feedback').empty();
+        }
+    });
     $(document).keydown(function (e) {
+        if (currentLetter == e.key) {
+            $('#feedback').append('<span class="glyphicon glyphicon-certificate"></span>');
+            correct = correct += 1;
+
+        } else if (e.which == 16) {
+
+        } else {
+            $('#feedback').append('<span class="glyphicon glyphicon-remove"></span>');
+            mistake = mistake += 1;
+        }
+
+
+    });
+    $(document).keydown(function (e) {
+
         if (e.which == 16) {
             $("#keyboard-upper-container").show();
             $("#keyboard-lower-container").hide();
 
         } else {
             if (lettersIndex < currentSentence.length) {
-            lettersIndex++;
-            currentLetter = letters[lettersIndex]; 
-            $('#target-letter').text(currentLetter);
-            blockPosition += 17.5 
-            $('#yellow-block').css("left", blockPosition);
+                lettersIndex++;
+                currentLetter = letters[lettersIndex];
+                $('#target-letter').text(currentLetter);
+                blockPosition += 17.5
+                $('#yellow-block').css("left", blockPosition);
+
             }
             if (lettersIndex == currentSentence.length) {
-                $('#sentence').text(" "); 
-                sentencesIndex ++; 
+                $('#sentence').text(" ");
+                sentencesIndex++;
                 currentSentence = sentences[sentencesIndex];
                 $('#sentence').text(currentSentence);
-                lettersIndex = 0; 
+                lettersIndex = 0;
                 letters = currentSentence.split('', currentSentence.length)
                 currentLetter = letters[lettersIndex];
                 $('#target-letter').text(currentLetter);
                 blockPosition = 0;
-                $('#yellow-block').css("left", blockPosition) 
+                $('#yellow-block').css("left", blockPosition)
+
             }
-            
+
+
         };
     });
 
@@ -69,6 +105,7 @@ $(document).ready(function () {
         });
 
     });
+
 
 
 
